67bfcd64d0efec946f1936dd959c2ce68cb0bf20
A <a@localhost.localdomain>
2021-07-02 18:46:33 +0200

    setting up user persistence

--- server/index.js
+++ server/index.js
@@ -5,14 +5,37 @@ const io = require("socket.io")(httpServer, {
   }
 });
 
+const crypto = require("crypto");
+const randomId = () => crypto.randomBytes(8).toString("hex");
+
+const { InMemorySessionStore } = require("./sessionStore");
+const sessionStore = new InMemorySessionStore();
+
+
+
 io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+		const session = sessionS>tore.findSession(sessionID)
+		if(session) {
+			socket.sessionID = sessionID;
+			socket.userID = session.userID;
+			socket.username = session.username
+			return next();
+		}
+  }
+
   const username = socket.handshake.auth.username;
   if (!username) {
     return next(new Error("invalid username"));
   }
+	socket.sessionID = randomId();
+	socket.userID = randomId();
   socket.username = username;
   next();
+
 });
+
 
 io.on("connection", socket => {
   // fetch existing users
